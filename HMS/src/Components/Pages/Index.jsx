e2107952dde8f36a645a499497ed697cc14d3c8f
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-01 23:25:00 +0700

    Update

--- HMS/src/Components/Pages/Index.jsx
+++ HMS/src/Components/Pages/Index.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Index = () => {
     return (
-        <div>Index</div>
+       // Hero
+       <header className="pt-5">
+        <div className="container">
+            <div className="row align-items-center">
+                <div className="col-lg-7">
+                    <div className="hero-content">
+                        <h1>Enjoy A Luxuary Experience</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </header>
     )
 }
 
