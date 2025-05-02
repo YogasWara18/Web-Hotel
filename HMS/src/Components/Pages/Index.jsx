ae26cac7111d2eda70c5cd5c3b9731453b4917c5
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-02 09:13:46 +0700

    Update

--- HMS/src/Components/Pages/Index.jsx
+++ HMS/src/Components/Pages/Index.jsx
@@ -9,6 +9,20 @@ const Index = () => {
                 <div className="col-lg-7">
                     <div className="hero-content">
                         <h1>Enjoy A Luxuary Experience</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quas iste consequatur sed! Officia odio iusto quisquam, quam dolor aut!</p>
+                        <button className="btn">Visit Us</button>
+                    </div>
+                </div>
+                <div className="col-lg-5">
+                    <div className="booking-form bg-white p-5 rounded">
+                        <h2>Book A Room</h2>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="booking-box">
+                                    <p><img src={checkIcon} alt=""/> Check In Date</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
